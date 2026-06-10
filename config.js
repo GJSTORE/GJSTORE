7b74644d8135283265bf0d0fec5af44997d67dcd
GJSTORE <guilherme.juraci@protonmail.com>
2026-06-09 23:40:05 -0300

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// GJ Store — configuração central
-// Ao reimplantar o Apps Script, atualize APENAS esta linha:
-const GAS_URL = "https://script.google.com/macros/s/AKfycbwCrxIGGgL-EutEEvHm__qX301To-juCmN6hJAgUKQt9mMS5sCN_GIpAlWrrsJxe9Rg/exec";
