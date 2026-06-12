334abac7eb4b8c5bd24defe73af16f722389ee30
GJSTORE <guilherme.juraci@protonmail.com>
2026-06-12 14:37:03 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// GJ Store — configuração central
+// Ao reimplantar o Apps Script, atualize APENAS esta linha:
+const GAS_URL = "https://script.google.com/macros/s/AKfycbzh_YuvkmiSq0r0UUSzrPohhJFSKH4kFT-BG76N8Z5WHCCXLAm23bCl1RqiJ9hjPIC4/exec";
