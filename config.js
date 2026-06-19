bf973336f117b1c555a1bbc2e38f7229c86aad2b
GJSTORE <guilherme.juraci@protonmail.com>
2026-06-19 12:37:36 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // GJ Store — configuração central
 // Ao reimplantar o Apps Script, atualize APENAS esta linha:
-const GAS_URL = "https://script.google.com/macros/s/AKfycbyCmik0otXfQWFYEpHEocD3lg6APJFDGARO2_RlghJg9qvT0NY9ldzECT6zaAsiZCVp/exec;
+const GAS_URL = "https://script.google.com/macros/s/AKfycbwTkpIsjdjFGbYdi40x7o3pPCL05iu2_KzS9_fFINB__l0pi093NZwUzvuljJGuFW1C/exec";
 
 // URL pública do site — usada nos links de compartilhar produto
 const CFG_SITE_URL = "https://gjstore.github.io/GJSTORE/";
